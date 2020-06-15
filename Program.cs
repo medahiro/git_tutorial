744a309db2198c34a592dceaa917523d7939fb93
medahiro <misatoedahiro@gmail.com>
2020-06-15 14:52:43 +0900

    表示内容を変更

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace work
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sum = 0;
+            
+            for (int i = 0; i < 10; i++) {
+                sum += i + 1;
+            }
+            
+            Console.WriteLine(sum);
         }
     }
 }
